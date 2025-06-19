0292cac99eb02810010866ef782ac93fea1b1ce8
Seiha <naingseiha@gmail.com>
2025-06-19 15:21:21 +0200

    fix: set base for Vite to fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // 👈 Add this line
   plugins: [react(), tailwindcss()],
 });
